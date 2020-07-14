4ff80996f0384846be5280429d5700918639a43f
nk2303 <ngankhong03@gmail.com>
2020-07-13 17:13:25 -0700

    countUniqueValues patterns

---
+++ MultiplePointersPattern/CountUniqueValues.js
@@ -0,0 +1,28 @@
+//count how many different values in the given SORTED array
+
+//My solution: Make object with a counter that counts everytime new key add to object.
+function countUniqueValues(arr){
+    let counter = { 'count': 0 };
+    for (let e of arr) {
+      if (!counter[e]) {
+        counter[e] = 1;
+        counter['count']++ ;
+      } else { counter[e] += 1 }
+    }
+    return counter['count']
+}
+
+//Better solution without using hash as data:
+
+function countUniqueValues(arr){
+    if (arr.length === 0) {return 0}
+    let i = 0
+    for ( let j = 1; j < arr.length; j++) {
+        if (arr[i] !== arr[j]) {
+            arr[i] = arr[j]
+            i++
+        } else {j++}
+        
+    }
+    return i+1
+}
