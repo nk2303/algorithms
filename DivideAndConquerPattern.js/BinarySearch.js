4164691ef7190e1b4cbfc4b412d9611573bc4509
nk2303 <ngankhong03@gmail.com>
2020-07-18 19:12:41 -0700

    binary search

---
+++ DivideAndConquerPattern.js/BinarySearch.js
@@ -0,0 +1,14 @@
+//Given array and a number, find that number's index in the array, return -1 if not found
+function search(array, val) {
+    let min = 0;
+    let max = array.length - 0;
+    while (min <= max){
+        let middle = Math.floor((min+max)/2);
+        if ( array[middle] > val ) {
+            min = middle + 1;
+        } else if ( array[middle] < val) {
+            max = middle -1;
+        } else { return array }
+    }
+    return -1
+}
