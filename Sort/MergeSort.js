8d1a7e66f2518eb53e11c8608d150267249ba717
nk2303 <ngankhong03@gmail.com>
2020-08-30 18:58:51 -0700

    merge sort

---
+++ Sort/MergeSort.js
@@ -0,0 +1,8 @@
+//Breaking array into smaller arrays, sort the arrays together.
+//I dont' really see how this is faster yet.
+const mergeSort = (arr) => {
+    for(let i = 1; i < arr.length; i++){
+        console.log(i)
+    }
+    return arr
+}
