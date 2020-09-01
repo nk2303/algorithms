09cc75fd6a56277d5dd3e0e293d83153eb4ef250
nk2303 <ngankhong03@gmail.com>
2020-08-31 18:46:37 -0700

    quick sort

--- Sort/MergeSort.js
+++ Sort/MergeSort.js
@@ -1,5 +1,10 @@
 //Breaking array into smaller arrays, sort the arrays together.
 //I dont' really see how this is faster yet.
+//Best and worst case: O(nlogn) always cost same time.
+
+//write a helpder funtionc
+
+const merge = () => {}
 const mergeSort = (arr) => {
     for(let i = 1; i < arr.length; i++){
         console.log(i)
